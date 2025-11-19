b6cb05ac81b4cd9e866ee94cc4a30862531fb6dd
Judith Breaud <judithbreaud@gmail.com>
2025-11-19 19:08:43 +0100

    update: add save_raw_matches and load_raw_matches in order to limit the calls to the API

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -1,6 +1,7 @@
 
 from src.api import api_get
-
+import json
+import os
 
 def fetch_matches(competition_id: int, season_id: int):
     """
@@ -14,6 +15,26 @@ def fetch_matches(competition_id: int, season_id: int):
         params={"season": season_id}
     )
 
-#def save_raw_matches(matches_json,file_path="data/raw/matches_2015_2025.json"):
+def save_raw_matches(matches_json,folder_path="data/raw/"):
+    """
+    Sauvegarde la réponse JSON brute dans un fichier structuré :
+    data/raw/FL1_2025.json
+    """
+    season_id=matches_json["filters"]['season']
+    competition_code=matches_json["competition"]["code"] #FL1 pour ligue 1
+    os.makedirs(folder_path, exist_ok=True)
+    filename = f"{competition_code}_{season_id}.json"
+    full_path = os.path.join(folder_path, filename)
+
+    with open(full_path, "w", encoding="utf-8") as f:
+        json.dump(matches_json, f, indent=2, ensure_ascii=False)
 
 
+def load_raw_matches(folder_path="data/raw/",competition_code="FL1",season_id=2025):
+    """
+    Charge un fichier JSON brut préalablement sauvegardé.
+    """
+    filename = f"{competition_code}_{season_id}.json"
+    full_path = os.path.join(folder_path, filename)
+    with open(full_path, "r", encoding="utf-8") as f:
+        json.load(f)
