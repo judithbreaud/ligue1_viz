459f3d2b65893165283e2d44938290f6b9b7a189
Judith Breaud <judithbreaud@gmail.com>
2025-11-20 14:03:08 +0100

    new: pipeline to fetch from the API and process the data

---
+++ src/pipelines/update_data.py
@@ -0,0 +1,29 @@
+from src.etl import fetch_matches, save_raw_matches, classement_interactif
+import pandas as pd
+import os
+
+COMPETITION_ID=2015 #ligue 1
+SEASON_ID=2025
+
+def main():
+    print("== Fetching data from API ==")
+    matches_json=fetch_matches(COMPETITION_ID,SEASON_ID)
+
+    print("== Saving raw data ==")
+    raw_path=save_raw_matches(matches_json)
+
+    print(f"== Raw data saved at: {raw_path} ==")
+
+    print("== Transforming data ==")
+    df = classement_interactif(matches_json)
+
+    os.makedirs("data/processed", exist_ok=True)
+    processed_path = "data/processed/standings_long.parquet"
+
+    print(f"== Saving processed data to {processed_path} ==")
+    df.to_parquet(processed_path, index=False)
+
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
