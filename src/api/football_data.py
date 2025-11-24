02dfc6fb40df252a88617dd5d3c5822c54198a17
Judith Breaud <judithbreaud@gmail.com>
2025-11-24 16:49:18 +0100

    remove debug prints

--- src/api/football_data.py
+++ src/api/football_data.py
@@ -10,14 +10,6 @@ import json
 API_TOKEN = os.getenv("FOOTBALL_DATA_TOKEN", "").strip()
 if not API_TOKEN:
     raise ValueError("FOOTBALL_DATA_TOKEN n'est pas défini")
-# Debug : vérifie que la variable est bien lue
-
-
-
-print("== DEBUG API ==")
-print(f"[DEBUG] Token length: {len(API_TOKEN)}")
-print(f"[DEBUG] Token prefix: {API_TOKEN[:6]}***")
-#print(f"[DEBUG] Headers prepared: {HEADERS}")
 
 def api_get(path, params=None, timeout=10):
     # Base URL
@@ -26,9 +18,6 @@ def api_get(path, params=None, timeout=10):
     # Prépare le header
     headers = {"X-Auth-Token": API_TOKEN}
     url = base.rstrip("/") + "/" + path.lstrip("/")
-    print(f"[DEBUG] URL: {url}")
-    print(f"[DEBUG] Params: {params}")
-    print(f"[DEBUG] Headers preview: {{'X-Auth-Token': '{API_TOKEN[:4]}***'}}")
     
     r = requests.get(url, headers=headers, params=params, timeout=timeout)
     
