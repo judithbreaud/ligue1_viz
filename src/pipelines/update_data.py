76e1fedbb8d81e02d31433455431d86b4774baf1
Judith Breaud <judithbreaud@gmail.com>
2026-01-20 12:08:22 +0100

    adding the prediction and elo updating to update data

--- src/pipelines/update_data.py
+++ src/pipelines/update_data.py
@@ -3,6 +3,9 @@ load_dotenv()
 from src.etl import fetch_matches, save_raw_matches, classement_interactif, find_next_opponent,save_next_opponent
 import pandas as pd
 import os
+from src.features import match_features_pred, update_elo_history_with_matchday,elo_dict_to_df
+from src.etl import load_raw_matches
+import joblib
 
 COMPETITION_ID="FL1" #ligue 1
 SEASON_ID="2025"
@@ -29,7 +32,64 @@ def main():
     opponent=find_next_opponent(matches_json)
     save_next_opponent(opponent_name=opponent)
 
+    print("== Import and repare data for prediction ==")
+    df_matches_25 = pd.json_normalize(matches_json["matches"])
+    history_elo = pd.read_parquet("data/processed/elos_history.parquet")
 
+    last_elo_md = history_elo[history_elo["season"].str.contains("2025")]["matchday"].max()
+    last_finished_md = (
+        df_matches_25[df_matches_25["status"] == "FINISHED"]["matchday"].max()
+    )
+    print(f"Last finished matchday: {last_finished_md}")
+    print(f"Last ELO matchday: {last_elo_md}")
+    if last_elo_md==last_finished_md+1:
+        print("No new matches, exiting pipeline")
+        return
+    elif last_elo_md==last_finished_md:
+        #update elo then do prediction
+        print("== Update ELO history ==")
+
+        new_history_elo,elos_end=update_elo_history_with_matchday(history_elo,df_matches_25[df_matches_25["matchday"]==last_finished_md])
+        print(1)
+
+        os.makedirs("data/processed", exist_ok=True)
+        processed_path = "data/processed/elos_history.parquet"
+
+        print(f"== Saving processed data to {processed_path} ==")
+        new_history_elo.to_parquet(processed_path, index=False)
+
+        elos_end_path = "data/processed/last_elo.parquet"
+        elos_end_df = elo_dict_to_df(
+            elos_end,df_matches_25,last_finished_md+1
+            )
+        elos_end_df.to_parquet(elos_end_path, index=False)
+        
+        X_pred,y_none,label_pred=match_features_pred(df_matches_25,new_history_elo,last_finished_md+1) 
+
+        print("== Import model ==")
+        model=joblib.load("models/match_prediction")
+
+        print(f"Predicting matchday: {last_finished_md + 1}")
+        y_proba = model.predict_proba(X_pred)
+        proba_df = pd.DataFrame(
+            y_proba,
+            columns=model.classes_
+        )
+        label_pred = label_pred.reset_index(drop=True)
+        final_df_pred = pd.concat([label_pred, proba_df], axis=1)
+
+        print("== Save prediction ==")
+
+        os.makedirs("data/processed", exist_ok=True)
+        processed_pred_path = "data/processed/next_matchday_prediction.parquet"
+
+        final_df_pred.to_parquet(processed_pred_path, index=False)
+
+
+
+    else: 
+        #there is an error 
+        print("There is an error")
 
     print("Done.")
 
